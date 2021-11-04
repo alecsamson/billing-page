2b5a3104fecf87898251faf61efdbcf8317dc20e
Alex Samson <samsonioanalexandru@gmail.com>
2021-11-04 14:02:53 +0200

    added _app and _document files for Mui setup

--- pages/invoices/[invoice].jsx
+++ pages/invoices/[invoice].jsx
@@ -2,7 +2,46 @@ import { useRouter } from "next/router"
 
 export default function Invoice() {
     const router = useRouter();
-    return <h1>
-        This is your invoice number: {router.query.invoice}.
-    </h1>
+    return (
+    <>
+        <h1>
+         October   
+        </h1>
+        <div>
+            Invoice number 
+            {router.query.invoice}
+            Due Date
+            21/10/2020
+        </div>
+        <div>
+            <h3>
+                Plan Details 
+            </h3>
+            <h3>
+                Amount
+            </h3>
+        </div>
+        <div>
+            <h4>
+                ILIMITADA 30
+            </h4>
+            <p>
+                21/09/2020 - 21/10/2020
+            </p>
+            <p>
+                49,99
+            </p>
+            <hr />
+        </div>
+        <h2>
+            Sub Total
+        </h2>
+        <h2>
+            49,99
+        </h2>
+        <h3>
+            VIEW INVOICE AS PDF
+        </h3>
+    </>
+    )
 }
