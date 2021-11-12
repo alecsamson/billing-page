1e4b5077440b120a128e2c71fe0c40e18462298e
Alex Samson <samsonioanalexandru@gmail.com>
2021-11-12 17:27:12 +0200

    current [invoice] progress

--- pages/invoices/[invoice].jsx
+++ pages/invoices/[invoice].jsx
@@ -1,20 +1,34 @@
 import { useRouter } from "next/router";
-import { Box, Divider, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export default function Invoice({ href, name }) {
   const router = useRouter();
   return (
-    <Grid container>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <ArrowBackIcon />
-        <Typography>October</Typography>
-      </Box>
+    <Grid container sx={{}} justifyContent="space-around">
       <Grid
         container
-        sx={{ background: "linear-gradient(90deg, #26A1B7 0%, #0372AE 100%)" }}
+        sx={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          p: 2.5,
+        }}
       >
-        <Typography component="div" align="center" sx={{ color: "white" }}>
+        <ArrowBackIcon />
+        <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+          October
+        </Typography>
+      </Grid>
+      <Grid
+        container
+        sx={{
+          background: "linear-gradient(90deg, #26A1B7 0%, #0372AE 100%)",
+          p: 2.5,
+        }}
+        justifyContent="space-between"
+      >
+        <Typography component="div" sx={{ color: "white" }}>
           <Typography variant="body1">Invoice number</Typography>
           <Typography variant="body2">{router.query.invoice}</Typography>
         </Typography>
@@ -25,27 +39,55 @@ export default function Invoice({ href, name }) {
         </Typography>
       </Grid>
 
-      <Grid item sx={{ flexDirection: "column" }}>
+      <Grid
+        item
+        container
+        justifyContent="space-between"
+        sx={{ padding: "16px 8px 0 16px", fontWeight: "bold" }}
+      >
         <Typography gutterBottom variant="h8">
           Plan details
         </Typography>
-        <Typography variant="body1" gutterBottom>
-          ILIMITADA 30
-        </Typography>
-        <Typography variant="body2">21/09/2020 - 21/10/2020</Typography>
-      </Grid>
-
-      <Grid item sx={{}}>
         <Typography variant="h8"> Amount </Typography>
-        <Typography variant="subtitle1">$19.00</Typography>
       </Grid>
 
-      <Grid item container sx={{ borderTop: "1px solid black" }}>
-        <Typography variant="h7">Sub Total</Typography>
-        <Typography variant="h7">49,99&euro;</Typography>
+      <Grid container justifyContent="space-between" sx={{ p: 2 }}>
+        <Grid item>
+          <Typography variant="body1" gutterBottom>
+            ILIMITADA 30
+          </Typography>
+          <Typography variant="body2">21/09/2020 - 21/10/2020</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="subtitle1">$49.00</Typography>
+        </Grid>
       </Grid>
 
-      <Typography sx={{ border: "1px solid black", borderRadius: "10px" }}>
+      <Grid
+        item
+        container
+        sx={{
+          borderTop: "2px solid #e7e7e7",
+          m: 5,
+          fontWeight: "bold",
+          paddingTop: "25px",
+          fontSize: "25px",
+          p: 2,
+        }}
+        justifyContent="space-between"
+      >
+        <Typography variant="h8">Sub Total</Typography>
+        <Typography variant="h8">49,99&euro;</Typography>
+      </Grid>
+
+      <Typography
+        sx={{
+          border: "1px solid black",
+          borderRadius: "30px",
+          padding: "15px 25rem",
+          fontWeight: "bold",
+        }}
+      >
         VIEW INVOICE AS PDF
       </Typography>
     </Grid>
