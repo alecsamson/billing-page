ac6bfa6f5d8bdafb115f6465aeccbba49bb8d6a4
Alex Samson <samsonioanalexandru@gmail.com>
2021-11-15 18:23:04 +0200

    setup initial sidebar icon styling

--- components/DesktopSidebar.jsx
+++ components/DesktopSidebar.jsx
@@ -1,5 +1,9 @@
 import { Container, Grid, Typography } from "@mui/material";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ContactSupportOutlinedIcon from "@mui/icons-material/ContactSupportOutlined";
+import PowerSettingsNewOutlinedIcon from "@mui/icons-material/PowerSettingsNewOutlined";
 
 export default function DesktopSidebar() {
   return (
@@ -33,16 +37,53 @@ export default function DesktopSidebar() {
         </Grid>
       </Grid>
 
-      <Typography sx={{ display: "flex", alignItems: "center" }}>
+      <Typography
+        sx={{
+          padding: "16px",
+
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
         <HomeOutlinedIcon />
         HOME
       </Typography>
-      <Typography sx={{ display: "flex", alignItems: "center" }}>
+      <Typography
+        sx={{
+          padding: "16px",
+          display: "flex",
+          alignItems: "center",
+          background: "#F2F5F8",
+          boxShadow:
+            "inset 0px -1px 0px rgba(0, 0, 0, 0.15), inset 3px 0px 0px #04819E",
+        }}
+      >
+        <AccountBalanceWalletOutlinedIcon />
         BILLING
       </Typography>
-      <Typography>ACCOUNT SETTINGS</Typography>
-      <Typography>HELP & FAQ</Typography>
-      <Typography>LOG OUT</Typography>
+      <Typography
+        sx={{ padding: "16px", display: "flex", alignItems: "center" }}
+      >
+        <AccountCircleOutlinedIcon />
+        ACCOUNT SETTINGS
+      </Typography>
+      <Typography
+        sx={{
+          padding: "16px",
+          display: "flex",
+          alignItems: "center",
+          boxShadow: "inset 0px -1px 0px rgba(0, 0, 0, 0.15) ",
+        }}
+      >
+        <ContactSupportOutlinedIcon />
+        HELP & FAQ
+      </Typography>
+      <Typography
+        sx={{ padding: "16px", display: "flex", alignItems: "center" }}
+      >
+        <PowerSettingsNewOutlinedIcon />
+        LOG OUT
+      </Typography>
     </Grid>
   );
 }
