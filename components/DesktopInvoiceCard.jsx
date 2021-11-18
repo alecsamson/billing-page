7f5ded41f34d1f769ac2ebf6303ae51133b525ec
Alex Samson <samsonioanalexandru@gmail.com>
2021-11-18 14:36:14 +0200

    finished spacing my invoices el, initial desktop i

---
+++ components/DesktopInvoiceCard.jsx
@@ -0,0 +1,5 @@
+import { Grid, Typography, Container } from "@mui/material";
+
+export default function DesktopInvoiceCard() {
+  return <Grid container></Grid>;
+}
--- components/MyInvoices.jsx
+++ components/MyInvoices.jsx
@@ -1,35 +1,51 @@
 import InvoiceCard from "./InvoiceCard";
 import { Container, Grid, Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 
 export default function Invoices(props) {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <Grid
       container
       sx={{
-        "&>*+*": { marginTop: "16px" },
         background: "#f2f5f8",
-        p: 3,
       }}
       direction="column"
       alignItems="center"
       justifyContent="center"
     >
-      <Typography variant="body1" align="center" gutterBottom>
-        <strong>My invoices</strong>
-      </Typography>
-      {props.invoiceIds.map((number) => {
-        return (
-          <InvoiceCard
-            key={number.id}
-            id={number.id}
-            price={number.price}
-            status={number.status}
-            date={number.date}
-            color={number.statusColor}
-            href="/invoices/[invoice]"
-          />
-        );
-      })}
+      <Grid
+        item
+        sx={{
+          "&>*+*": { marginTop: "16px" },
+          p: 3,
+          width: matches ? "100%" : "70%",
+        }}
+      >
+        <Typography
+          variant="body1"
+          align="left"
+          gutterBottom
+          sx={{ marginLeft: matches ? "2rem" : "5rem" }}
+        >
+          <strong>My invoices</strong>
+        </Typography>
+        {props.invoiceIds.map((number) => {
+          return (
+            <InvoiceCard
+              key={number.id}
+              id={number.id}
+              price={number.price}
+              status={number.status}
+              date={number.date}
+              color={number.statusColor}
+              href="/invoices/[invoice]"
+            />
+          );
+        })}
+      </Grid>
     </Grid>
   );
 }
