4bc6171c58a9a13bf4d0bb7fb0879c2b1361027f
Alex Samson <samsonioanalexandru@gmail.com>
2021-11-19 10:35:07 +0200

    removed nextJS Link in favor of useRouter hook

--- components/InvoiceCard.jsx
+++ components/InvoiceCard.jsx
@@ -1,59 +1,59 @@
 import { Grid, Typography, Container } from "@mui/material";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function InvoiceCard(props) {
+  const { push } = useRouter();
   return (
-    <Link href={props.href} passHref>
-      <Container>
-        <Grid
-          container
-          sx={{
-            p: 2,
-            margin: "auto",
-            maxWidth: "100%",
-            flexGrow: 1,
-            boxShadow: 1,
-            position: "relative",
-            background: "#ffffff",
-          }}
-          justifyContent="space-between"
-        >
-          <Grid item>
-            <Typography gutterBottom variant="subtitle1">
-              Invoice {props.id}
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              <strong>October</strong>
-            </Typography>
-            <Typography variant="body2">
-              Due on <strong>{props.date}</strong>
-            </Typography>
-          </Grid>
-
-          <Typography
-            variant="subtitle1"
-            sx={{
-              position: "absolute",
-              color: "white",
-              textTransform: "uppercase",
-              fontWeight: "bold",
-              right: 0,
-              backgroundColor: props.color,
-              borderTopLeftRadius: "10px",
-              borderBottomLeftRadius: "10px",
-              padding: "0 8px",
-            }}
-          >
-            {props.status}
+    <Container onClick={() => push(props.href)}>
+      <Grid
+        container
+        sx={{
+          p: 2,
+          margin: "auto",
+          maxWidth: "100%",
+          flexGrow: 1,
+          boxShadow: 1,
+          position: "relative",
+          background: "#ffffff",
+        }}
+        justifyContent="space-between"
+      >
+        <Grid item>
+          <Typography gutterBottom variant="subtitle1">
+            Invoice {props.id}
           </Typography>
-          <Grid item sx={{ alignSelf: "flex-end" }}>
-            <Typography variant="subtitle1">
-              {props.price}
-              {"\u20AC"}
-            </Typography>
-          </Grid>
+          <Typography variant="body1" gutterBottom>
+            <strong>October</strong>
+          </Typography>
+          <Typography variant="body2">
+            Due on <strong>{props.date}</strong>
+          </Typography>
         </Grid>
-      </Container>
-    </Link>
+
+        <Typography
+          variant="subtitle1"
+          sx={{
+            position: "absolute",
+            color: "white",
+            textTransform: "uppercase",
+            fontWeight: "bold",
+            right: 0,
+            backgroundColor: props.color,
+            borderTopLeftRadius: "10px",
+            borderBottomLeftRadius: "10px",
+            padding: "0 8px",
+          }}
+        >
+          {props.status}
+        </Typography>
+        <Grid item sx={{ alignSelf: "flex-end" }}>
+          <Typography variant="subtitle1">
+            {props.price}
+            {"\u20AC"}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
