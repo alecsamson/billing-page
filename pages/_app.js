2073fbd0f50a532b4228d0d1743600f073734c33
Alex Samson <samsonioanalexandru@gmail.com>
2021-12-15 15:11:45 +0200

    you know what it is

--- pages/_app.js
+++ pages/_app.js
@@ -7,53 +7,6 @@ import "../styles/globals.css";
 import axios from "axios";
 const theme = createTheme();
 
-// const invoiceNumbers = [
-//   {
-//     id: 123455123455,
-//     price: 49.99,
-//     status: "due",
-//     date: "21/12/2021",
-//   },
-//   {
-//     id: 234567234567,
-//     price: 79.99,
-//     status: "due",
-//     date: "21/11/2021",
-//   },
-//   {
-//     id: 341678345678,
-//     price: 49.99,
-//     status: "due",
-//     date: "21/10/2021",
-//   },
-//   {
-//     id: 456589456789,
-//     price: 49.99,
-//     status: "paid",
-//     date: "21/09/2021",
-//   },
-//   {
-//     id: 547890567890,
-//     price: 49.99,
-//     status: "paid",
-//     date: "21/08/2021",
-//   },
-//   {
-//     id: 356789456789,
-//     price: 49.99,
-//     status: "paid",
-//     date: "21/07/2021",
-//   },
-//   {
-//     id: 267890567890,
-//     price: 49.99,
-//     status: "paid",
-//     date: "21/06/2021",
-//   },
-// ];
-// const api = axios.create({
-//   baseURL: `http://localhost:5000`,
-// });
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 function BillingApp({ Component, pageProps }) {
   React.useEffect(() => {
@@ -62,9 +15,6 @@ function BillingApp({ Component, pageProps }) {
       console.log("State updated to" + state);
     });
 
-    // api.get("/").then((res) => {
-    //   console.log(res.data);
-    // });
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
